b99d019dffd5166a381c9aef622fbd3002754897
ordinary1person <3531755625@qq.com>
2022-02-26 16:16:14 +0800

    practice

---
+++ python习题解答/1.3.py
@@ -0,0 +1,15 @@
+#从键盘输入身高和体重，计算BIM并判断是否健康(不支持循环输入)
+from tkinter import ROUND
+
+
+height = input('请输入你的身高(m)：')
+weight = input('请输入你的体重(kg)：')
+
+BIM = float(weight) / (float(height) ** 2)
+print('标准BIM；18.5~23.9')
+print('BIM:',round(BIM,2))
+
+if (BIM >= 18.5) and (BIM <= 23.9) :
+    print('健康状态：健 康')
+else :
+    print('健康状态：不健康')
