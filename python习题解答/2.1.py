95110637e535dd53c9fc42c053fcbfa38444a490
ordinary1person <3531755625@qq.com>
2022-02-27 12:33:56 +0800

    p

--- python习题解答/2.1.py
+++
@@ -1,6 +0,0 @@
-#输入摄氏度输出华氏度
-Celsius_degree = float(input('请输入摄氏度(℃ )：'))
-print('\n*************************')
-print('摄氏度：{} ℃'.format(round(Celsius_degree,1))) 
-Fahrenheit_degree = Celsius_degree*1.8 + 32
-print('华氏度：%.1f ℉' % Fahrenheit_degree)
